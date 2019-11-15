b78203d1167eb7571dfa74186cda0ebb827a7feb
asolaresojop2016 <asolaresojop2016@fau.edu>
2019-11-15 00:39:46 -0500

    edited links to correct php pages and updated nav bar

--- M4/index.php
+++ M4/index.php
@@ -12,53 +12,57 @@ echo '</pre>';
 
 <html lang="en">
 
-<head>
+	<head>
 
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="">
-  <meta name="author" content="">
+	  <meta charset="utf-8">
+	  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	  <meta name="description" content="">
+	  <meta name="author" content="">
 
-  <title>Campus Snapshots</title>
-  <!-- Bootstrap core CSS -->
-  <link href="css/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	  <title>Campus Snapshots</title>
+	  <!-- Bootstrap core CSS -->
+	  <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	  
+	  <!-- Custom styles for this template -->
+	  <link href="bootstrap/css/campussnapshot.css" rel="stylesheet">
 
-  <!-- Custom styles for this template -->
-  <link href="css/campussnapshot.css" rel="stylesheet">
+	  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	  
+	</head>
 
-</head>
 
-<body>
-	<ul class="topnav">
-	  <li><a class="active" href="index.php">Main Page</a></li>
-	  <li><a href="PostSnapshot.html">Post Snapshot</a></li>
-	  <li><a href="">Search Snapshot</a></li>
-	  <li><a href="">Update Snapshot</a></li>
-	  <li><a href="">Edit Account</a></li>
-    <li><a href="logout.php">Logout</a></li>
-	</ul>
-	<div class="container">
-		<div class="col-lg mx-auto">
-			<div class="logo mb-3">
-				<div class="col-lg text-center">
-					<h1>Welcome to Campus Snapshots</h1>
+	<body>
+		<ul class="topnav">
+		  <li><a class="active" href="index.php">Main Page</a></li>
+		  <li><a href="PostSnapshot.html">Post Snapshot</a></li>
+		  <li><a href="SearchSnapshot.html">Search Snapshot</a></li>
+		  <li><a href="UpdateSnapshot.html">Update Snapshot</a></li>
+		  <li><a href="EditAccount.php">Edit Account</a></li>
+		  <li class="right"><a href="logout.php"><i class="fa fa-sign-out"></i>  Logout</a></li>
+		</ul>
+		<div class="container">
+			<div class="col-lg mx-auto">
+				<div class="logo mb-3">
+					<div class="col-lg text-center">
+						<h1>Welcome to Campus Snapshots</h1>
+						<h5>Current User: <?php echo $_SESSION['username'] ?></h5>
+					</div>
 				</div>
 			</div>
+	<!--populate with posts table when defined
+	 <?php
+	$sql = "select * from USERS";
+	$res = $dbcon->query($sql);
+
+	echo "<table>"; // print table data from posts table
+
+	while($row = mysqli_fetch_array($res)){   //Creates a loop to loop through results
+	echo "<tr><td>" . $row['tbd'] . "</td><td>" . $row['tbd'] . "</td></tr>";  //$row['index'] the index here is a field name
+	}
+
+	echo "</table>"; //Close the table in HTML
+	?> 
+	-->
 		</div>
-<!--populate with posts table when defined
- <?php
-$sql = "select * from USERS";
-$res = $dbcon->query($sql);
-
-echo "<table>"; // print table data from posts table
-
-while($row = mysqli_fetch_array($res)){   //Creates a loop to loop through results
-echo "<tr><td>" . $row['tbd'] . "</td><td>" . $row['tbd'] . "</td></tr>";  //$row['index'] the index here is a field name
-}
-
-echo "</table>"; //Close the table in HTML
-?> -->
-
-	</div>
-</body>
+	</body>
 </html>
--- M4/register.php
+++ M4/register.php
@@ -3,66 +3,68 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
+	<head>
 
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="">
-  <meta name="author" content="">
+	  <meta charset="utf-8">
+	  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	  <meta name="description" content="">
+	  <meta name="author" content="">
 
-  <title>Campus Snapshot</title>
+	  <title>Campus Snapshots</title>
 
-  <!-- Bootstrap core CSS -->
-  <link href="css/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	  <!-- Bootstrap core CSS -->
+	  <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	  
+	  <!-- Custom styles for this template -->
+	  <link href="bootstrap/css/campussnapshot.css" rel="stylesheet">
 
-  <!-- Custom styles for this template -->
-  <link href="css/campussnapshot.css" rel="stylesheet">
+	  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
-</head>
+	</head>
 
-<body>
-  <div class="container">
-    <div class="col-md-5 mx-auto">
-		<div class="myform form ">
-			<div class="logo mb-3">
-				 <div class="col-md-12 text-center">
-						<h1>Registration</h1>
-				 </div>
+	<body>
+	  <div class="container">
+		<div class="col-md-5 mx-auto">
+			<div class="myform form ">
+				<div class="logo mb-3">
+					 <div class="col-md-12 text-center">
+							<h1>Register</h1>
+					 </div>
+				</div>
+				<form method="post" action="register.php" name="register">
+					<div class="form-group">
+						<input type="hidden" name="register_user" value="register_user">
+						<label>Email Address</label>
+						<input type="email" name="email"  class="form-control" id="email" placeholder="Enter Email" value="<?php echo $email; ?>">
+					</div>
+					<div class="form-group">
+						<label>First name</label>
+						<input type="First Name" name="firstname"  class="form-control" id="firstname" placeholder="Enter First Name" value="<?php echo $firstname; ?>">
+					</div>
+					<div class="form-group">
+						<label>Last Name</label>
+						<input type="Last Name" name="lastname"  class="form-control" id="lastname" placeholder="Enter Last Name" value="<?php echo $lastname; ?>">
+					</div>
+					<div class="form-group">
+						<label>Password</label>
+						<input type="password" name="password" id="password"  class="form-control" placeholder="Enter Password">
+					</div>
+					<div class="col-md-12 text-center ">
+						<button type="submit" name="register_user" value="register_user" class="btn btn-block mybtn btn-primary tx-tfm">Register</button>
+					</div>
+					<div class="col-md-12 ">
+						<div class="login-or">
+						<hr class="hr-or">
+						<span class="span-or">or</span>
+						</div>
+					</div>
+					<div class="form-group">
+						<p class="text-center">Already have account? <a href="login.php" >Login here</a></p>
+					</div>
+				</form>
 			</div>
-            <form method="post" action="register.php">
-				<div class="form-group">
-                    <input type="hidden" name"reigister_user" value="register_user">
-                    <label>Email</label>
-                    <input type="email" name="email"  class="form-control" id="email" placeholder="Enter Email" value="<?php echo $email; ?>">
-                </div>
-                <div class="form-group">
-                    <label>First name</label>
-                    <input type="First Name" name="firstname"  class="form-control" id="firstname" placeholder="Enter First Name" value="<?php echo $firstname; ?>">
-                </div>
-                <div class="form-group">
-                    <label>Last Name</label>
-                    <input type="Last Name" name="lastname"  class="form-control" id="lastname" placeholder="Enter Last Name" value="<?php echo $lastname; ?>">
-                </div>
-                <div class="form-group">
-                    <label>Password</label>
-                    <input type="password" name="password" id="password"  class="form-control" placeholder="Enter Password">
-                </div>
-                <div class="col-md-12 text-center ">
-                    <button type="submit" name="register_user" value="register_user" class="btn btn-block mybtn btn-primary tx-tfm">Register</button>
-                </div>
-				<div class="col-md-12 ">
-                    <div class="login-or">
-                    <hr class="hr-or">
-                    <span class="span-or">or</span>
-                    </div>
-                </div>
-                <div class="form-group">
-                    <p class="text-center">Already have account? <a href="login.html" >Login here</a></p>
-                </div>
-            </form>
-        </div>
-	</div>
-  </div>
-</body>
+		</div>
+	  </div>
+	</body>
 
 </html>
